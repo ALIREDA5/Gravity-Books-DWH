58b032787d243f37b2cd058cf254af476d1f4c5b
Ali Reda <123907150+ALIREDA5@users.noreply.github.com>
2026-02-15 10:38:57 +0200

    Refactor SQL script to include database context
    Added 'USE gravity_books' statement and 'GO' command for SQL execution context.

--- Source Scripts/Orders_accumlating_fact.sql
+++ Source Scripts/Orders_accumlating_fact.sql
@@ -1,3 +1,5 @@
+use gravity_books
+go
 WITH cte_order_tracking AS (
     SELECT [order_id], [1] AS orderReceived_date_id, [2] AS orderDelivered_date_id, [3] AS orderCancelled_date_id, [4] AS orderReturned_date_id
     FROM (
@@ -16,8 +18,7 @@ cte_last_status AS (
            ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY date_id DESC) AS rn
     FROM order_history
 )
-
-
+go
 select c.order_id, c.dest_address_id, c.shipping_method_id, c.date_id as order_date, c.customer_id, l.book_id, l.price, h.orderReceived_date_id, h.orderReturned_date_id, h.orderDelivered_date_id, h.orderCancelled_date_id, ls.LastStatusID
 from cust_order c join order_line l
 on c.order_id = l.order_id
@@ -25,4 +26,5 @@ join cte_order_tracking h
 on c.order_id = h.order_id
 JOIN cte_last_status ls
 ON h.order_id = ls.order_id
+
 WHERE ls.rn = 1;
